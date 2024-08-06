1839a1f305d983bea15acbbba906a27bc856ecf5
Vinod Pittampalli <vinod@pittampalli.com>
2024-08-06 15:21:44 +0000

    fix: Replace AnonymousAuthenticationProvider with custom authProvider

--- app/db.ts
+++ app/db.ts
@@ -1,18 +1,23 @@
+import 'server-only'
 import { Client } from '@microsoft/microsoft-graph-client';
-import { auth, EnrichedSession } from '../auth';
-import { AnonymousAuthenticationProvider } from '@microsoft/kiota-abstractions';
+import { auth, EnrichedSession } from '../auth'; // Replace './auth' with the correct path to the file containing the EnrichedSession type
+
 
 export default async function getGraphClient() {
-    const session = (await auth()) as EnrichedSession;
-    const accessToken = session?.accessToken;
 
-    const authProvider = new AnonymousAuthenticationProvider(async (request) => {
-        request.headers.set('Authorization', `Bearer ${accessToken}`);
-    });
+    const session = (await auth()) as EnrichedSession;
+    // console.log('Session inside the route ', session);
+
+    const accessToken = session?.accessToken;
+    // const refreshToken = session?.refreshToken; // Remove this line if refreshToken is not used
 
     const client = Client.init({
-        authProvider: authProvider,
+        authProvider: (done) =>
+            done(
+                null,
+                accessToken // WHERE DO WE GET THIS FROM?
+            ),
     });
 
-    return client;
+    return client 
 }
