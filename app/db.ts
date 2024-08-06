34e7915ae38af83c7aef7135149b676349e5767d
Vinod Pittampalli <vinod@pittampalli.com>
2024-08-06 18:56:54 +0000

    fix: Remove unused imports and code from db.ts

--- app/db.ts
+++ app/db.ts
@@ -1,35 +1,23 @@
 import 'server-only'
 import { Client } from '@microsoft/microsoft-graph-client';
-import { auth, EnrichedSession } from '../auth';
-import { MailFolder, Message } from "@microsoft/microsoft-graph-types";
+import { auth, EnrichedSession } from '../auth'; // Replace './auth' with the correct path to the file containing the EnrichedSession type
+
 
 export default async function getGraphClient() {
+
     const session = (await auth()) as EnrichedSession;
+    // console.log('Session inside the route ', session);
+
     const accessToken = session?.accessToken;
+    // const refreshToken = session?.refreshToken; // Remove this line if refreshToken is not used
 
     const client = Client.init({
         authProvider: (done) =>
             done(
                 null,
-                accessToken
+                accessToken // WHERE DO WE GET THIS FROM?
             ),
     });
 
     return client 
-}
-
-export async function getEmailFolders(): Promise<MailFolder[]> {
-  const client = await getGraphClient();
-  const response = await client.api('/me/mailFolders').get();
-  return response.value;
-}
-
-export async function getMessagesForFolder(folderName: string, folderId: string): Promise<Message[]> {
-  const client = await getGraphClient();
-  const response = await client.api(`/me/mailFolders/${folderId}/messages`)
-    .select('subject,from,receivedDateTime,bodyPreview')
-    .top(50)
-    .orderBy('receivedDateTime DESC')
-    .get();
-  return response.value;
 }
